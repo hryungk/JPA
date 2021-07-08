b35d3f7fef874df68874180e89f17607d70666b1
HoppyCat <hryungk@gmail.com>
2021-07-08 16:35:33 -0700

    Transformed into a JPA project.
    Model, Repository, Service classes are added.

--- src/main/java/org/perscholas/springmvctest/controllers/HomeController.java
+++ src/main/java/org/perscholas/springmvctest/controllers/HomeController.java
@@ -1,4 +1,4 @@
-package org.perschholas.springmvctest.controllers;
+package org.perscholas.springmvctest.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
---
+++ src/main/java/org/perscholas/springmvctest/repositories/EmployeeRepository.java
@@ -0,0 +1,27 @@
+package org.perscholas.springmvctest.repositories;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.perscholas.springmvctest.models.Employee;
+import org.springframework.stereotype.Repository;
+
+@Repository // basically DAO
+public class EmployeeRepository {
+
+	public Employee findEmployeeById(int id) {
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("Seattle_SpringMVC");
+		EntityManager entityManager = emfactory.createEntityManager();
+		Employee employee = entityManager.find(Employee.class, id);
+		entityManager.close();
+		emfactory.close();
+		return employee;
+	}
+}
