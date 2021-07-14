ea9e8bfe1f751a1119c8b46bb65a3ddc899bb266
HoppyCat <hryungk@gmail.com>
2021-07-14 15:22:47 -0700

    07/13/21 Adding more web components

--- src/main/java/org/perscholas/springmvctest/controllers/HomeController.java
+++ src/main/java/org/perscholas/springmvctest/controllers/HomeController.java
@@ -1,8 +1,11 @@
 package org.perscholas.springmvctest.controllers;
 
+import java.time.LocalDate;
+
 import org.perscholas.springmvctest.models.Employee;
 import org.perscholas.springmvctest.services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -28,7 +31,10 @@ public class HomeController {
 	 * Connecting the JSP and model.
 	 */
 	@PostMapping("/search") // Match the form's action name
-	public String searchEmployeeByNumber(@RequestParam("employeeNumber") Integer employeeNumber, Model model) {
+	public String searchEmployeeByNumber(@RequestParam("employeeNumber") Integer employeeNumber, 
+			@RequestParam("dateInput") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date, 
+			Model model) {
+		System.out.println(date);
 		Employee employee = employeeService.findEmployeeById(employeeNumber);
 		System.out.println(employee);
 		model.addAttribute("employee", employee);
